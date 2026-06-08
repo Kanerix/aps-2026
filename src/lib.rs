c0563e183e9261e2ff7b8ae2594c04ef99bb5301
Kasper Jønsson <kas@lerpz.com>
2026-06-08 16:43:44 +0200

    Removed not needed algos

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,8 @@ pub mod bfs;
 /// # Depth-first search
 pub mod dfs;
 
-/// # Dijkstra's algorithm
-pub mod dijkstra;
-
-/// # Fenwick tree
-pub mod fenwick;
-
 /// # Maximum flow
 pub mod maxflow;
-
-/// # Random number
-pub mod random;
 
 /// # Trie
 pub mod trie;
